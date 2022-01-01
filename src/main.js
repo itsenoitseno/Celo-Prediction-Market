b3d9c3dd8802db747edef36a8ce7f1e7c384ad73
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-01-01 22:13:02 +0100

    made changes to contract and frontend adjustments

--- src/main.js
+++ src/main.js
@@ -5,14 +5,7 @@ import {
 import BigNumber from "bignumber.js"
 import predictionMarketAbi from '../contract/predictionMarket.abi.json'
 import erc20Abi from "../contract/erc20.abi.json"
-
-const ERC20_DECIMALS = 18
-const PMContractAddress = "0x968b3FE0e3B3BE96F81f5502865484E42cAb4eCf"
-
-//An old CA.
-//Const PMContractAddress = "0xae41b9C2E400f289c393432FBAf912F1FfFa3956"
-
-const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+import {cUSDContractAddress, ERC20_DECIMALS, PMContractAddress} from "./utils/constants";
 
 let candidates = []
 let contract
@@ -212,9 +205,9 @@ function renderBettingModal(id) {
   notificationOff()
 }
 
+// opt the ui rendering
 function bettingModalTemplate(_candidate) {
   return `
-  <div class="modal-content">
     <div class="modal-header">
         <h5 class="modal-title" id="placeBetModal">Place Bet</h5>
         <button
@@ -258,7 +251,6 @@ function bettingModalTemplate(_candidate) {
         Close
         </button>
     </div>
-  </div>
   `
 }
 
@@ -352,8 +344,6 @@ document.querySelector("#bettingModalDisplay").addEventListener("click", async (
   if (e.target.className.includes("placeBet")) {
     $('#betModal').modal('hide');
     const ID = e.target.id;
-    console.log(ID)
-    console.log(candidates[ID])
     const amount = new BigNumber(document.getElementById("bet").value).shiftedBy(ERC20_DECIMALS).toString();
 
     let isApproved = true;
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const ERC20_DECIMALS = 18
+const PMContractAddress = "0x968b3FE0e3B3BE96F81f5502865484E42cAb4eCf"
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+
+export { ERC20_DECIMALS, PMContractAddress, cUSDContractAddress }
